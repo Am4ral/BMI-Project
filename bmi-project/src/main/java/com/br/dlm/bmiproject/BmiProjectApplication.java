c7f0363435fde9e0dac9f5fc0e6e3037c38a902d
Danilo Chagas <danilovga1000@gmail.com>
2024-04-29 14:07:26 -0300

    back-end criado

---
+++ bmi-project/src/main/java/com/br/dlm/bmiproject/BmiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.br.dlm.bmiproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmiProjectApplication.class, args);
+	}
+
+}
---
+++ bmi-project/src/test/java/com/br/dlm/bmiproject/BmiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dlm.bmiproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
