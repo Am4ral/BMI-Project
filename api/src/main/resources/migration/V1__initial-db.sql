fe98163c647a6fd6bf8306e6a4902509ca29af85
danilochagasc <danilovga1000@gmail.com>
2024-04-30 18:51:27 -0300

    adicionando migrations

---
+++ api/src/main/resources/migration/V1__initial-db.sql
@@ -0,0 +1,38 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
+-- noinspection SqlDialectInspectionForFile
+
+-- Tabela role
+CREATE TABLE role (
+    id_role SERIAL PRIMARY KEY,
+    authority VARCHAR(20) NOT NULL
+);
+
+-- Tabela users
+CREATE TABLE users (
+    id_users BIGSERIAL PRIMARY KEY,
+    name VARCHAR(80) NOT NULL,
+    email VARCHAR(60) NOT NULL,
+    password VARCHAR(45) NOT NULL,
+    idRole BIGINT,
+    FOREIGN KEY (idRole) REFERENCES role(id_role)
+);
+
+-- Tabela pacient
+CREATE TABLE pacient (
+     id_pacient BIGSERIAL PRIMARY KEY,
+     name VARCHAR(80) NOT NULL,
+     birthdate DATE,
+     gender CHAR(1) NOT NULL
+);
+
+-- Tabela measurement
+CREATE TABLE measurement (
+     id_measurement BIGSERIAL PRIMARY KEY,
+     height FLOAT NOT NULL,
+     weight FLOAT NOT NULL,
+     bmi FLOAT,
+     measurementDate DATE,
+     id_pacient BIGINT,
+     FOREIGN KEY (id_pacient) REFERENCES pacient(id_pacient)
+);
