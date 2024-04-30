78c03e3a6d6b73d7ebd92ac243f4e3462929e055
danilochagasc <danilovga1000@gmail.com>
2024-04-30 20:19:14 -0300

    iniciando projeto

--- api/src/main/java/com/br/dlm/api/data/entity/Pacient.java
+++ api/src/main/java/com/br/dlm/api/data/entity/Pacient.java
@@ -1,4 +1,20 @@
 package com.br.dlm.api.data.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "pacient")
 public class Pacient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_pacient")
+    private Long idPacient;
+
 }
