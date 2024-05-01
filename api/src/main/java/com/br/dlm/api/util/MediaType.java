0e5342b9a9c2d78356963d82dc650cb769a65c72
Danilo Chagas <danilovga1000@gmail.com>
2024-05-01 00:02:02 -0300

    arquivo utilitario para guardar tipos de midia

---
+++ api/src/main/java/com/br/dlm/api/util/MediaType.java
@@ -0,0 +1,7 @@
+package com.br.dlm.api.util;
+
+public class MediaType {
+
+    public static final String APPLICATION_JSON = "application/json";
+    public static final String TEXT_PLAIN = "text/plain";
+}
