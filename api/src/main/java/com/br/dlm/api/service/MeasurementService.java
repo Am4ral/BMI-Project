8f56704549c4d26cff475cdd7d7469f678d15a27
Danilo Chagas <danilovga1000@gmail.com>
2024-05-01 00:02:09 -0300

    criado service de medidas

--- api/src/main/java/com/br/dlm/api/service/MeasurementService.java
+++ api/src/main/java/com/br/dlm/api/service/MeasurementService.java
@@ -1,9 +1,11 @@
 package com.br.dlm.api.service;
 
+import com.br.dlm.api.data.dto.request.MeasurementRequestDTO;
 import com.br.dlm.api.data.dto.response.MeasurementResponseDTO;
 import com.br.dlm.api.data.entity.Measurement;
 import com.br.dlm.api.exceptions.general.EntityNotFoundException;
 import com.br.dlm.api.repository.MeasurementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,14 +14,35 @@ import java.util.stream.Collectors;
 @Service
 public class MeasurementService {
 
+    @Autowired
     private MeasurementRepository measurementRepository;
 
-    private MeasurementResponseDTO findById(Long id) {
+    public MeasurementResponseDTO findById(Long id) {
         return new MeasurementResponseDTO(getMeasurementById(id));
     }
 
-    private List<MeasurementResponseDTO> getAllMeasurements() {
+    public List<MeasurementResponseDTO> getAllMeasurements() {
         return measurementRepository.findAll().stream().map(MeasurementResponseDTO::new).collect(Collectors.toList());
+    }
+
+    public MeasurementResponseDTO createMeasurement(MeasurementRequestDTO measurementRequestDTO) {
+        Measurement measurement = new Measurement(measurementRequestDTO);
+        return new MeasurementResponseDTO(measurementRepository.save(measurement));
+    }
+
+    public MeasurementResponseDTO updateMeasurement(Long id, MeasurementRequestDTO measurementRequestDTO) {
+        Measurement updatedMeasurement = getMeasurementById(id);
+        updatedMeasurement.setHeight(measurementRequestDTO.height());
+        updatedMeasurement.setWeight(measurementRequestDTO.weight());
+        updatedMeasurement.setBmi(updatedMeasurement.calculateBmi());
+        updatedMeasurement.setPacient(measurementRequestDTO.pacient());
+
+        return new MeasurementResponseDTO(measurementRepository.save(updatedMeasurement));
+    }
+
+    public String deleteMeasurement(Long id) {
+        measurementRepository.deleteById(id);
+        return "Measurement with id " + id + " deleted successfully";
     }
 
     private Measurement getMeasurementById(Long id) {
